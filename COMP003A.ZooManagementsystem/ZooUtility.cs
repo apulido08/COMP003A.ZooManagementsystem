9f9acb847881719ae2c067c6f0baf089c9fded94
Alvaro Pulido <alavropulido@my.whccd.edu>
2025-02-20 19:21:02 -0800

    added zoo utility tab to project

---
+++ COMP003A.ZooManagementsystem/ZooUtility.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace COMP003A.ZooManagementsystem
+{
+    internal class ZooUtility
+    {
+        public static void DescribeAnimal(string name)
+        {
+            System.Console.WriteLine($"Name: {name}");
+        }
+
+        public static void DescribeAnimal(string name, string species)
+        {
+            DescribeAnimal(name);
+            Console.WriteLine($"Species: {species}");
+        }
+
+        public static void DescribeAnimal(string name, string species, int age)
+        {
+            DescribeAnimal(name, species);
+            Console.WriteLine($"Age: {age}");
+        }
+    }
+}
